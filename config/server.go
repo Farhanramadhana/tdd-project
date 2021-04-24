183ccc8dabef36fdf008346ea442ee78a482d825
farhanramadhana <farhanramadhana97@gmail.com>
2021-04-24 16:44:35 +0700

    update server config

--- config/server.go
+++ config/server.go
@@ -11,29 +11,17 @@ type Options struct {
 }
 
 type Server struct {
-	ginServer *gin.Engine
-	options Options
-}
-
-func (s *Server) Router(method string, path string, handler func(*gin.Context)) {
-	if method == "GET" {
-		s.ginServer.GET(path, handler)
-	} else if method == "POST" {
-		s.ginServer.POST(path, handler)
-	} else if method == "PUT" {
-		s.ginServer.PUT(path, handler)
-	}else {
-		fmt.Sprintf("method don't allowed")
-	}
+	GinServer  *gin.Engine
+	options    Options
 }
 
 func New(options Options) *Server {
 	return &Server{
-		ginServer: gin.Default(),
+		GinServer: gin.Default(),
 		options:   options,
 	}
 }
 
 func (s *Server) Start() {
-	s.ginServer.Run(fmt.Sprintf(":%v", s.options.Port))
+	s.GinServer.Run(fmt.Sprintf(":%v", s.options.Port))
 }
