a7f010bafbadec0524db4ba22f48b7bf35fe2cad
farhanramadhana <farhanramadhana97@gmail.com>
2021-04-24 16:46:44 +0700

    update main.go

--- main.go
+++ main.go
@@ -2,15 +2,42 @@ package main
 
 import (
 	"bootcamp/config"
+	"bootcamp/entity"
+	"bootcamp/services/auth"
 	"bootcamp/services/user"
-
+	"fmt"
 )
 
 func main() {
 	s := config.New(config.Options{Port: 50001})
 
-	s.Router("POST", "/user", user.CreateUserHandler())
-	s.Router("GET", "/user", user.GetAllUserHandler())
+	// predifines admin user
+	superAdmin := entity.RegistrationUserEntity{
+		FullName: "super admin",
+		Email:    "superadmin@admin.com",
+		Password: "admin12345",
+		Role:     "admin",
+	}
 
+	var userService user.UserServiceInterface = user.UserService{}
+	createUser, err := userService.CreateUser(superAdmin)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	fmt.Print(createUser)
+	authorized := s.GinServer.Group("/")
+	authorized.Use(auth.LoginHandler())
+	{
+		// admin only
+		authorized.POST("/user", user.CreateUserHandler())
+		authorized.DELETE("/user/:id", user.DeleteUserById())
+		// authorized.PUT("/user", user.CreateUserHandler())
+
+		// all user type
+		authorized.GET("/user", user.GetAllUserHandler())
+		authorized.GET("/user/username/:username", user.GetUserByUsernameHandler())
+		authorized.GET("/user/email/:email", user.GetUserByEmailHandler())
+		authorized.GET("/user/id/:id", user.GetUserByIdHandler())
+	}
 	s.Start()
 }
