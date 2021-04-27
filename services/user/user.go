247dc899f7f4716e1392709c7207bb0e58d7b133
farhanramadhana <farhanramadhana97@gmail.com>
2021-04-27 11:45:12 +0700

    add updateUserById and rename some functions name

--- services/user/user.go
+++ services/user/user.go
@@ -4,6 +4,7 @@ import (
 	"bootcamp/config"
 	"bootcamp/entity"
 	"bootcamp/repository"
+	"errors"
 	"fmt"
 	"math/rand"
 	"strconv"
@@ -17,9 +18,10 @@ type UserServiceInterface interface {
 	CreateUser(data entity.RegistrationUserEntity) (entity.DataUserEntity, error)
 	GetAllUser() ([]entity.DataUserEntity, bool)
 	GetUserByUsername(username string) (entity.DataUserEntity, bool)
-	GetUserById(id string) (entity.DataUserEntity, bool)
+	GetUserByID(id string) (entity.DataUserEntity, bool)
 	GetUserByEmail(email string) (entity.DataUserEntity, bool)
-	DeleteUserById(id string) (entity.DataUserEntity, bool)
+	DeleteUserByID(id string) (entity.DataUserEntity, bool)
+	UpdateUserByID(id string, data entity.UpdateUserEntity) (entity.DataUserEntity, error)
 }
 
 type UserService struct {
@@ -60,30 +62,93 @@ func (service UserService) CreateUser(data entity.RegistrationUserEntity) (entit
 	return dataUser, nil
 }
 
-// GetAllUser func is used to retrieve all data user
+// GetAllUser func is used to retrieve all user data
 func (service UserService) GetAllUser() ([]entity.DataUserEntity, bool) {
 	userData, isExist := service.repository.GetAllUser()
 	return userData, isExist
 }
 
+// GetUserByUsername func is used to retrieve specific user data by username
 func (service UserService) GetUserByUsername(username string) (entity.DataUserEntity, bool) {
 	userData, isExist := service.repository.GetUserByUserName(username)
 	return userData, isExist
 }
 
-func (service UserService) GetUserById(id string) (entity.DataUserEntity, bool) {
-	userData, isExist := service.repository.GetUserById(id)
+// GetUserById func is used to retrieve specific user data by user id
+func (service UserService) GetUserByID(id string) (entity.DataUserEntity, bool) {
+	userData, isExist := service.repository.GetUserByID(id)
 	return userData, isExist
 }
 
+// GetUserByEmail func is used to retrieve specific user data by email
 func (service UserService) GetUserByEmail(email string) (entity.DataUserEntity, bool) {
 	userData, isExist := service.repository.GetUserByEmail(email)
 	return userData, isExist
 }
 
-func (service UserService) DeleteUserById(id string) (entity.DataUserEntity, bool) {
-	userData, isExist := service.repository.DeleteUserById(id)
+// DeleteUserByID func is used to delete specific user data by id
+func (service UserService) DeleteUserByID(id string) (entity.DataUserEntity, bool) {
+	userData, isExist := service.repository.DeleteUserByID(id)
 	return userData, isExist
+}
+
+// UpdateUserByID func is used to update specific user data by id
+func (service UserService) UpdateUserByID(id string, data entity.UpdateUserEntity) (entity.DataUserEntity, error) {
+	var validate config.Validator
+	var user entity.DataUserEntity
+
+	v := validate.NewValidator()
+	err := v.Validate(data)
+
+	if err != nil {
+		return entity.DataUserEntity{}, err
+	}
+
+	existingData, isExist := service.repository.GetUserByID(id)
+
+	if !isExist {
+		return entity.DataUserEntity{}, errors.New("User not found")
+	}
+
+	user.ID = existingData.ID
+	if data.FullName != "" {
+		var initialName = existingData.InitialName
+		var userName = existingData.Username
+
+		name := SplitFullName(data.FullName)
+		initialName = CreateInitialName(data.FullName)
+		
+		if existingData.FirstName != name[0] || existingData.LastName != name[2] {
+			userName = GenerateUserName(name)
+		}
+
+		user.FirstName = name[0]
+		user.MiddleName = name[1]
+		user.LastName = name[2]
+		user.InitialName = initialName
+		user.Username = userName
+	}
+
+	data.Password = existingData.Password
+	if data.Password != "" {
+		password := EncryptPassword(data.Password)
+		user.Password = password
+	}
+
+	user.Role = existingData.Role
+	if data.Role != "" && existingData.Role != data.Role {
+		user.Role = data.Role
+	}
+
+	user.Email = existingData.Email
+	if data.Email != "" && existingData.Email != data.Email {
+		user.Email = data.Email
+	}
+
+	user.UpdatedAt = time.Now().Format(time.RFC3339)
+
+	userData := service.repository.UpdateUserByID(id, user)
+	return userData, nil
 }
 
 // SplitFullName is func to split fullname to first, middle, and last of Name
@@ -157,6 +222,7 @@ func GenerateID() string {
 	return id
 }
 
+// EncryptPassword is func to encryp password using bcrypt
 func EncryptPassword(password string) string {
 	pwd := []byte(password)
 	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
