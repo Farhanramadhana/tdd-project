57195646ff845a55d297fabf895d72569433941d
farhanramadhana <farhanramadhana97@gmail.com>
2021-04-27 11:46:13 +0700

    add UpdateUserEntity

--- entity/userEntity.go
+++ entity/userEntity.go
@@ -8,6 +8,14 @@ type RegistrationUserEntity struct {
 	Role     string `validate:"required"`
 }
 
+// UpdateUserEntity is entity for updating data user
+type UpdateUserEntity struct {
+	FullName string `json:"fullname"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Role     string `json:"role"`
+}
+
 // DataUserEntity is entity for database column
 type DataUserEntity struct {
 	ID          string
